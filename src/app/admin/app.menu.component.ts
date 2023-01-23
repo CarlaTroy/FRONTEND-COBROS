b1838bc187934011f092f0c4d67be208befa76d3
Carla-Troya <troyacarla0@gmail.com>
2023-01-23 01:24:52 -0500

    tabla maquetada

--- src/app/admin/app.menu.component.ts
+++ src/app/admin/app.menu.component.ts
@@ -25,8 +25,8 @@ export class AppMenuComponent implements OnInit {
                             {
                                 label: 'Gestionar', icon: 'pi pi-fw pi-bookmark',
                                  routerLink: ['/admin/course']
-                            },
-                        ]
+                            }
+                        ],
                     },
                     {
                         label: 'Parroquias',
