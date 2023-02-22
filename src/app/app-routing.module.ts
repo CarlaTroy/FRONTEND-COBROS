e01f84034ef28acf37982371c2cdb8a53f7917e6
Carla Troya <99460507+CarlaTroy@users.noreply.github.com>
2023-02-22 02:52:54 -0500

    guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AdminComponent } from './admin/admin.component';
 import { AppLayoutComponent } from './layout/app.layout.component';
+import { AuthGuard } from './guard/auth.guard';
 
 
 @NgModule({
@@ -14,23 +15,14 @@ import { AppLayoutComponent } from './layout/app.layout.component';
             //{path: '', component: AdminComponent},
             { path: "admin",component:AdminComponent,children:[
                 { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                //{ path: 'usuario', loadChildren: () => import('./admin/usuario/usuario.module').then(m => m.UsuarioModule) },
                 { path: 'user', loadChildren: () => import('./admin/user/user.module').then(m => m.UserModule) },
                 { path: 'student', loadChildren: () => import('./admin/student/student.module').then(m => m.StudentModule) },
                 { path: 'enrollement', loadChildren: () => import('./admin/enrollement/enrollement.module').then(m => m.EnrollementModule) },
-                //{ path: 'produccion', loadChildren: () => import('./admin/produccion/produccion.module').then(m => m.ProduccionModule) },
-                //{ path: 'resultados', loadChildren: () => import('./admin/resultados/resultados.module').then(m => m.ResultadosModule) },
-                //{ path: 'costo-produccion', loadChildren: () => import('./admin/costo-produccion/costo-produccion.module').then(m => m.CostoProduccionModule) },
+
                 { path: 'course', loadChildren: () => import('./admin/course/course.module').then(m => m.CourseModule) },
                 { path: 'cohorte', loadChildren: () => import('./admin/cohorte/cohorte.module').then(m => m.CohorteModule) },
                 { path: 'report', loadChildren: () => import('./admin/report/report.module').then(m => m.ReportModule) },
-                //{ path: 'productores', loadChildren: () => import('./admin/productores/productores.module').then(m => m.ProductoresModule) },
-                //{ path: 'intermediario-produccion', loadChildren: () => import('./admin/intermediario-produccion/intermediario-produccion.module').then(m => m.IntermediarioModule) },
-                //{ path: 'intermediario', loadChildren: () => import('./admin/intermediario/intermediario.module').then(m => m.IntermediarioModule) },
-                //{ path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-                //{ path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-                //{ path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) },
-            ]},
+            ],canActivate: [AuthGuard]},
               {
 
                 path: 'xx', component: AppLayoutComponent,
