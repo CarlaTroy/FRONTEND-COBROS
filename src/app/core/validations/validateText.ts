08836b25250afeb117c74039bd888c189a696a91
CarlaTroy <99460507+CarlaTroy@users.noreply.github.com>
2023-02-24 00:51:47 -0500

    filtros y cambio en tablas
    Agregar filtros a reportes y actualizar la tabla estudiante

--- src/app/core/validations/validateText.ts
+++ src/app/core/validations/validateText.ts
@@ -1,6 +1,6 @@
 export function soloLetras(e:any):any {
   let caracter=e.key;
-  let caracterValido=/^[a-zA-ZáéíóúñÁÉÍÓÚÑ]+$/.test(caracter);
+  let caracterValido=/^[a-zA-ZáéíóúñÁÉÍÓÚÑ ]+$/.test(caracter);
   if(caracterValido){
     return true;
   }
