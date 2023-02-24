6ea63b42003d5ed02f2550128d74c06595785d4d
Carla Troya <troyacarla0@gmail.com>
2023-02-24 02:16:35 -0500

    reporte ok

---
+++ src/app/admin/report/report.ts
@@ -0,0 +1,10 @@
+export interface ReportDTOFullMapper{
+    id:number,
+    nombresCompletos:string,
+    cursoCohorte:string,
+    valorTotal:number,
+    saldoPendiente:number,
+    cuotas:string,
+    estado:string,
+    pensiones:string,
+}
